0b396eebdc059d6d8d5ed225b3964cc56cb6c9e7
leving <336382@gmail.com>
2025-10-29 16:04:35 +0200

    feat(coin): add rotation animation
    Added continuous rotation to make coins visually dynamic.

--- Assets/Scripts/Collectables/Coin.cs
+++ Assets/Scripts/Collectables/Coin.cs
@@ -6,12 +6,18 @@ namespace SmallBallBigPlane.Collectables
     public class Coin : MonoBehaviour, ICollectable
     {
        [Inject] private CoinManager _coinManager;
-        
+       [SerializeField] private float rotationSpeed = 10f;
+       
         public void Collect()
         {
             _coinManager.CollectCoin();
             
             gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
         }
     }
 }
